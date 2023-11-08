d96668cbd14c562ba17f0ebb12e32fd2afba2008
brrkak <128439902+brrkak@users.noreply.github.com>
2023-11-08 19:38:51 +0900

    couner생성

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import { legacy_createStore as createStore } from "redux";
+
+const add = document.getElementById("plus");
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
+
+number.innerText = 0;
+
+const ADD = "ADD";
+const MINUS = "MINUS";
+
+const countModifier = (count = 0, action) => {
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return;
+  }
+};
+
+const countStore = createStore(countModifier);
+
+const onChange = () => {
+  number.innerText = countStore.getState();
+};
+
+countStore.subscribe(onChange);
+
+add.addEventListener("click", () => countStore.dispatch({ type: ADD }));
+minus.addEventListener("click", () => countStore.dispatch({ type: MINUS }));
